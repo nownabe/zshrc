270a6b85a7d6576cdc5c01c29f9dd69745f8abc7
nownabe <nownabe@users.noreply.github.com>
2021-12-24 23:42:59 +0900

    :fire: Avoid from using zplug in this plugin

--- plugins.zsh
+++ plugins.zsh
@@ -1,17 +1,6 @@
-zplug 'mafredri/zsh-async'
-zplug 'sindresorhus/pure', use:pure.zsh, as:theme
-
-zplug 'zsh-users/zsh-syntax-highlighting', defer:2
-
-[[ -z "${CODESPACES}" ]] && zplug 'asdf-vm/asdf'
-
-if ! zplug check --verbose; then
-  zplug install
-fi
-
-zplug load --verbose
-
+# zsh-users/zsh-syntax-highlighting
 ZSH_HIGHLIGHT_HIGHLIGHTERS=(main brackets cursor)
 
+# asdf-vm/asdf
 [[ -e "${ZPLUG_REPOS}/asdf-vm/asdf/asdf.sh" ]] && source $ZPLUG_REPOS/asdf-vm/asdf/asdf.sh
 [[ -n "${ASDF_DIR}" ]] && fpath=(${ASDF_DIR}/completions $fpath)
