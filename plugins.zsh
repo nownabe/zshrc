b0e3cb1dbedd04c9f3152bc4b8b0b5d2e647be9d
nownabe <nownabe@gmail.com>
2025-04-22 16:49:06 +0900

    Disable asdf

--- plugins.zsh
+++ plugins.zsh
@@ -2,7 +2,7 @@
 ZSH_HIGHLIGHT_HIGHLIGHTERS=(main brackets)
 
 # asdf-vm/asdf
-[[ -e "${ZPLUG_REPOS}/asdf-vm/asdf/asdf.sh" ]] && source $ZPLUG_REPOS/asdf-vm/asdf/asdf.sh
-[[ -n "${ASDF_DIR}" ]] && fpath=(${ASDF_DIR}/completions $fpath)
+# [[ -e "${ZPLUG_REPOS}/asdf-vm/asdf/asdf.sh" ]] && source $ZPLUG_REPOS/asdf-vm/asdf/asdf.sh
+# [[ -n "${ASDF_DIR}" ]] && fpath=(${ASDF_DIR}/completions $fpath)
 
 :
